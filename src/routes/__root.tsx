e5cc7c36cd39fd918a148668a9e744677fa2ffd6
develowlper <d.schulz@hello-sunshine.dev>
2025-04-25 15:00:07 +0200

    feat: header layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,15 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import clsx from 'clsx';
 export const Route = createRootRoute({
   component: () => (
     <>
       <nav className="navbar shadow-sm bg-neutral text-neutral-content gap-4">
-        <img src="/poker.png" alt="logo" className="h-10 w-10" />
+        <div className="navbar-start flex gap-2">
+          <img src="/poker.png" alt="logo" className="h-10 w-10 navbar-start" />
+          <h1 className="text-lg">Planning Poker</h1>
+        </div>
         <div className="navbar-center flex gap-2">
           <Link to="/" className="link [&.active]:font-bold">
             Home
@@ -14,9 +18,16 @@ export const Route = createRootRoute({
             About
           </Link>
         </div>
+        <div className="navbar-end"></div>
       </nav>
+      <div
+        className={clsx(
+          'flex-1 flex flex-col justify-center items-center py-4 px-2',
+        )}
+      >
+        <div className="max-w-lg">{<Outlet />}</div>
+      </div>
 
-      <Outlet />
       <TanStackRouterDevtools />
       <ReactQueryDevtools initialIsOpen={false} />
     </>
